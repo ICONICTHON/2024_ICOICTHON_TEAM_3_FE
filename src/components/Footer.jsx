8a4c9d9f878e462134504cc7540c2daa87304708
Doyeon Kim <ehdus022191@dgu.ac.kr>
2024-11-14 03:29:26 +0900

    feat : switch pages

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,6 @@
 import PropTypes from 'prop-types'; // PropTypes 추가
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 import '../styles/footer.css';
 import checkTicketIcon from '../images/CheckTicket.png';
@@ -6,25 +8,36 @@ import homeIcon from '../images/Home.png';
 import favoriteIcon from '../images/Favorite.png';
 
 function Footer({ activeFooter, setActiveFooter }) {
+  const navigate = useNavigate();
+
+  const handleFooterClick = (section) => {
+    setActiveFooter(section);
+    if (section === '식권 확인') {
+      navigate('/food-ticket'); // 식권 확인 페이지로 이동
+    } else {
+      navigate('/'); // 메인 페이지로 이동
+    }
+  };
+
   return (
     <footer className="footer">
       <button
         className={`footer-btn ${activeFooter === '식권 확인' ? 'active' : ''}`}
-        onClick={() => setActiveFooter('식권 확인')}
+        onClick={() => handleFooterClick('식권 확인')}
       >
         <img src={checkTicketIcon} alt="식권확인" />
         <p>식권확인</p>
       </button>
       <button
         className={`footer-btn ${activeFooter === '홈' ? 'active' : ''}`}
-        onClick={() => setActiveFooter('홈')}
+        onClick={() => handleFooterClick('홈')}
       >
         <img src={homeIcon} alt="홈" />
         <p>홈</p>
       </button>
       <button
         className={`footer-btn ${activeFooter === '즐겨찾기' ? 'active' : ''}`}
-        onClick={() => setActiveFooter('즐겨찾기')}
+        onClick={() => handleFooterClick('즐겨찾기')}
       >
         <img src={favoriteIcon} alt="즐겨찾기" />
         <p>즐겨찾기</p>
@@ -40,4 +53,3 @@ Footer.propTypes = {
   activeFooter: PropTypes.string.isRequired,
   setActiveFooter: PropTypes.func.isRequired,
 };
-
