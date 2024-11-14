a921673049b3ff8067f4064b70dc9ff0d33ec219
Doyeon Kim <ehdus022191@dgu.ac.kr>
2024-11-14 23:51:10 +0900

    feat: search

--- src/App.jsx
+++ src/App.jsx
@@ -7,26 +7,40 @@ import TimeNav from './components/TimeNav';
 import RestaurantList from './components/RestaurantList';
 import Footer from './components/Footer';
 import FoodTicket from './components/FoodTicket';
+import axios from 'axios';
 
 
 function App() {
   const [selectedTime, setSelectedTime] = useState('점심');
   const [activeLang, setActiveLang] = useState('KOR');
   const [activeFooter, setActiveFooter] = useState('홈');
+  const [searchResults, setSearchResults] = useState([]); // 검색 결과 상태
+
+  // 검색 요청을 보내는 함수
+  const handleSearch = async (searchTerm) => {
+    try {
+      const response = await axios.get('${process.env.VITE_APP_API_URL}/restaurants', {
+        params: { search_menu: searchTerm },
+      });
+      setSearchResults(response.data);
+    } catch (error) {
+      console.error("검색 오류:", error);
+    }
+  };
 
   return (
     <Router>
       <div className="App">
         <Header activeLang={activeLang} setActiveLang={setActiveLang} />
         
-        <div className="content-wrapper"> {/* Footer와 겹치지 않도록 padding-bottom 추가 */}
+        <div className="content-wrapper">
           <Routes>
             <Route
               path="/"
               element={
                 <>
-                  <TimeNav selectedTime={selectedTime} setSelectedTime={setSelectedTime} />
-                  <RestaurantList />
+                  <TimeNav selectedTime={selectedTime} setSelectedTime={setSelectedTime} onSearch={handleSearch} />
+                  <RestaurantList searchResults={searchResults} />
                 </>
               }
             />
