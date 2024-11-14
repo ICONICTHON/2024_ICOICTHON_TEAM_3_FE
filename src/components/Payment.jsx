7d7bfba63913b97166287043c62d5de2f739f916
VegetableKim <kcl2502@gmail.com>
2024-11-15 01:46:31 +0900

    Feat: 카카오페이 연동 수정

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import '../styles/Payment.css';
-import Load from './Load';
-import KakaoPay from './KakaoPay';
+import React, { useState } from "react";
+import "../styles/Payment.css";
+import Load from "./Load";
+import KakaoPay from "./KakaoPay";
 
 function Payment({ isOpen, onClose, menuItem }) {
   const [isLoading, setIsLoading] = useState(false);
@@ -12,10 +12,34 @@ function Payment({ isOpen, onClose, menuItem }) {
   const handlePayment = () => {
     setIsLoading(true);
 
-    setTimeout(() => {
-      setIsLoading(false);
-      setShowKakaoPay(true);
-    }, 1000);
+    // 결제 처리 요청
+    const data = {
+      name: menuItem.item, // 카카오페이에 보낼 상품명
+      totalPrice: menuItem.price, // 카카오페이에 보낼 가격
+    };
+
+    let baseUrl = window.location.origin; // 현재 도메인 기반으로 URL 생성
+
+    $.ajax({
+      type: "POST",
+      url: `${baseUrl}/order/pay/ready`,
+      data: JSON.stringify(data),
+      contentType: "application/json",
+      success: function (response) {
+        setIsLoading(false);
+        // 성공 시 카카오페이 결제 화면으로 이동
+        location.href = response.next_redirect_pc_url;
+      },
+      error: function (error) {
+        setIsLoading(false);
+        console.error("결제 준비 실패", error);
+      },
+    });
+
+    //    setTimeout(() => {
+    //      setIsLoading(false);
+    //      setShowKakaoPay(true);
+    //    }, 1000);
   };
 
   const handleKakaoPayClose = () => {
