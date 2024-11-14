910e95812c45a7da7b9cd5d987d8d9913922d2ad
VegetableKim <kcl2502@gmail.com>
2024-11-15 05:05:57 +0900

    카카오페이 결제 replace 부분 수정

--- src/components/Payment.jsx
+++ src/components/Payment.jsx
@@ -14,7 +14,9 @@ function Payment({ isOpen, onClose, menuItem }) {
     setIsLoading(true);
 
     try {
-      const price = parseInt(menuItem.price.replace(/[^0-9]/g, ""), 10);
+      const price = menuItem?.price
+        ? parseInt(menuItem.price.replace(/[^0-9]/g, ""), 10)
+        : 99;
       const data = {
         name: menuItem.item || "상품명", // 상품명
         totalPrice: price || 20000, // 총 결제 금액
